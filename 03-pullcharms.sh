6335a2a36069c88d10929c097e92fc5d4c8c47f2
Fatih Nar <fenar@yahoo.com>
2017-06-26 10:06:23 -0500

    Delete 03-pullcharms.sh

--- 03-pullcharms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash/
-sudo mkdir /srv/data/charms
-cd /srv/data/charms
-charm pull cs:$distro/odl-controller $distro/odl-controller
-charm pull cs:~narindergupta/neutron-api-odl-11 $distro/neutron-api-odl
-charm pull cs:~narindergupta/openvswitch-odl-3 $distro/openvswitch-odl
-charm pull cs:~narindergupta/neutron-gateway-6 $distro/neutron-gateway
