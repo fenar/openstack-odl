dd5f019bd1bf7d4e80755dfbc51ff5bd3a117522
Fatih Nar <fenar@yahoo.com>
2017-06-26 09:43:56 -0500

    Update 01-deploy-model.sh

--- 01-deploy-model.sh
+++ 01-deploy-model.sh
@@ -5,10 +5,10 @@ model=`juju list-models |awk '{print $1}'|grep odl`
 if [[ ${model:0:3} == "odl" ]]; then
 	    echo "ODL model exists and reusing it !"
  	    juju switch odl
-     	juju deploy odl-mitaka.yaml
+     	juju deploy odl-ocata.yaml
 else
      	echo "ODL model does NOT exist and creating it!"
      	juju add-model odl
      	juju switch odl
-     	juju deploy odl-mitaka.yaml
+     	juju deploy odl-ocata.yaml
 fi
