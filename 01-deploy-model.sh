f98f9b9012c44f24ff00850b78104e1afe90fd77
fenar <fenar@yahoo.com>
2017-06-29 15:00:18 -0500

    Updates for Bundles.

--- 01-deploy-model.sh
+++ 01-deploy-model.sh
@@ -4,12 +4,12 @@
 model=`juju list-models |awk '{print $1}'|grep odl`
 
 if [[ ${model:0:3} == "odl" ]]; then
-	    echo "ODL model exists and reusing it !"
- 	    juju switch odl
-     	juju deploy odl-ocata.yaml
+        echo "ODL model exists and reusing it !"
+        juju switch odl
+     	juju deploy odl-mitaka-beryllium.yaml
 else
      	echo "ODL model does NOT exist and creating it!"
      	juju add-model odl
      	juju switch odl
-     	juju deploy odl-ocata.yaml
+     	juju deploy odl-mitaka-beryllium.yaml
 fi
